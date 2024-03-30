32ceec95e4f137cd1e3aac971890ee3efc61ae32
HUBSlover <ivanled2002@gmail.com>
2024-03-30 22:39:39 +0300

    finished-user-and-relations-between-entities

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { User } from '@prisma/client'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PrismaService } from 'src/prisma.service'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private configService: ConfigService,
+		private prisma: PrismaService
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET')
+		})
+	}
+
+	async validate({
+		id
+	}): Promise<
+		Pick<
+			{
+				id: number
+				createdAt: Date
+				updatedAt: Date
+				email: string
+				password: string
+				name: string
+				avatarPath: string
+				phone: string
+			},
+			'id'
+		>
+	> {
+		return this.prisma.user.findUnique({ where: { id: +id } })
+	}
+}
