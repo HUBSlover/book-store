84cc67cd27ef7b1ef3fc134295898689d9ea51df
HUBSlover <ivanled2002@gmail.com>
2024-03-30 13:53:45 +0300

    registration-authorisation-complete

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { PrismaService } from './prisma.service'
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+
+	const prismaService = app.get(PrismaService)
+	// await prismaService.enableShutDownHooks(app)
+
+	app.setGlobalPrefix('api')
+	app.enableCors()
+	await app.listen(4200)
+}
+
+bootstrap()
