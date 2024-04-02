dbd8a8ea154e33eb97362b0cb7e6044466d8a5f0
HUBSlover <ivanled2002@gmail.com>
2024-04-02 22:54:11 +0300

    pagination-and-statistics

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+	constructor(
+		private prisma: PrismaService,
+		private userSevice: UserService
+	) {}
+
+	async getMain(userId: number) {
+		const user = await this.userSevice.byId(userId, {
+			orders: {
+				select: {
+					items: {
+						select: {
+							price: true
+						}
+					}
+				}
+			},
+			reviews: true
+		})
+
+		/* REPLACE WITH SQL */
+
+		return user.orders
+
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},
+			{
+				name: 'Reviews',
+				value: user.reviews.length
+			},
+			{
+				name: 'Favorites',
+				value: user.favorites.length
+			},
+			{
+				name: 'Total amount',
+				value: 1000
+			}
+		]
+	}
+}
